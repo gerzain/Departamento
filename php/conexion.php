504967e3c127c2932cbaed203cdcc5a523ae2293
Irving <irving.java@gmail.com>
2017-02-21 19:20:32 -0600

    Revert "Inicial"
    This reverts commit 6db2b3f3f1750660b77b2d8476f0ab098edcf808.

--- php/conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conexion = mysql_connect('127.0.0.1', 'root', '');
-mysql_select_db('dep', $conexion);
-
-function fechaNormal($fecha){
-		$nfecha = date('d/m/Y',strtotime($fecha));
-		return $nfecha;
-}
-?>
